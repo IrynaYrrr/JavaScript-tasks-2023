2aa36f6aedf2150fe120878bfce6622a11f75570
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-02-19 19:25:05 +0200

    commit index1

---
+++ index1.js
@@ -0,0 +1,51 @@
+// function findLongestWord(string) {
+//   // Change code below this line
+//   const newArr = string.split(" ");
+//   let longWord = '';
+//   for (const word of newArr){
+//     if(word.lenght > longWord.length){
+//       longWord = word;
+//     }
+//     console.log(word.length)
+//   }
+//   ;
+//   return longWord;
+//   // Change code above this line
+// }
+// findLongestWord('The quick brown fox jumped over the lazy dog');
+
+// const arr = [1,2,3,4,5,6,7];
+// const result = arr.concat([8854,9], [10,1231] )
+// console.log(result);
+
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+// let total = 0;
+
+// for(let i = 0; i < numbers.length; i+=1) {
+//   if(numbers[i] % 2 === 0) {
+//     total+=numbers[i];
+//   }
+// }
+
+// console.log(total);
+
+// function fnA() {
+//   console.log("Лог внуртри функции fnA до вызова fnB");
+//   fnB();
+//   console.log("Лог внуртри функции fnA после вызова fnB");
+// }
+
+// function fnB() {
+//   console.log("Лог внутри функции fnB");
+// }
+
+// console.log("Лог перед вызовом fnA");
+// fnA();
+// console.log("Лог после вызова fnA");
+
+//("Лог перед вызовом fnA");
+//("Лог внуртри функции fnA до вызова fnB")
+//("Лог внутри функции fnB")
+//("Лог внуртри функции fnA после вызова fnB")
+//("Лог после вызова fnA")
